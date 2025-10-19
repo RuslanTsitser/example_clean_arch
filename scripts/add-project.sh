3c5af677e0223ab0ee212fef1046775181259c2e
RuslanTsitser <cicer_ruslan@mail.ru>
2025-10-19 19:17:45 +0300

    Add web platform support to sandbox project, including necessary files and configurations

--- scripts/add-project.sh
+++ scripts/add-project.sh
@@ -4,7 +4,7 @@ project_name=$1
 
 mkdir -p monorepo/apps/$project_name
 cd monorepo/apps/$project_name
-flutter create . --empty --org com.tsitser --project-name $project_name --platforms android,ios
+flutter create . --empty --org com.tsitser --project-name $project_name --platforms android,ios,web
 echo "Project $project_name created successfully"
 cd ../../..
 
--- scripts/create-simple-project.sh
+++ scripts/create-simple-project.sh
@@ -2,5 +2,5 @@
 
 project_name=$1
 
-flutter create . --empty --org com.tsitser --project-name $project_name --platforms android,ios
+flutter create . --empty --org com.tsitser --project-name $project_name --platforms android,ios,web
 echo "Project $project_name created successfully"
